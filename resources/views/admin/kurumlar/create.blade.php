4ee52b367d418562e7bd738427b61bbe3fceb582
Vural Kayra Cetintas <73638860+vuralkayracetintas@users.noreply.github.com>
2023-06-22 21:33:29 +0300

    kurum olusturma sayfasi duzenlendi

--- resources/views/admin/kurumlar/create.blade.php
+++ resources/views/admin/kurumlar/create.blade.php
@@ -3,57 +3,16 @@
 @section('content')
 <div class="content">
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-8">
+      
+      
 
-                @if(session('status'))
+      
+        @if(session('status'))
                 <div class="alert alert-primary" role="alert">
                     {{session('status')}}
                 </div>
                 @endif
-                <div class="card">
-                    <div class="card-header" data-background-color="purple">
-                        <h4 class="title">E-LooPsAkademi</h4>
-                        <p class="category">Yeni Kurum Olustur</p>
-                    </div>
-                    <div class="card-content">
-                        <form action="{{route('admin.kurumlar.ekle.post')}}" method="POST">
-                            {{csrf_field()}}
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group label-floating is-empty">
-                                        <label class="control-label">Yayin Evi Adi</label>
-                                        <input type="text" name="name" class="form-control">
-                                        <span class="material-input"></span>
-                                    </div>
-                                </div>
-                            </div>
-                            <button type="submit" class="btn btn-primary pull-right">Yayin Evi Guncelle</button>
-                            <div class="clearfix"></div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card card-profile">
-                    <div class="card-avatar">
-                        <a href="#pablo">
-                            <img class="img" src="{{asset('admin_css/assets/img/faces/marc.jpg')}}">
-                        </a>
-                    </div>
-                    <div class="content">
-                        <h6 class="category text-gray">CEO / Co-Founder</h6>
-                        <h4 class="card-title">Alec Thompson</h4>
-                        <p class="card-content">
-                            Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...
-                        </p>
-                        <a href="#pablo" class="btn btn-primary btn-round">Follow</a>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header" data-background-color="purple">
                     <h4 class="title">Deneme Ui</h4>
